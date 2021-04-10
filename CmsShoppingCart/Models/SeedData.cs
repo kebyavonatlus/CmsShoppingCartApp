b6d63f6aaf13c1d3de98b0133f85836929eea96c
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-04-10 17:00:38 +0600

    Adding seed data

---
+++ CmsShoppingCart/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using CmsShoppingCart.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace CmsShoppingCart.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider provider)
+        {
+            using (var context = new CmsShoppingCartContext
+                (provider.GetRequiredService<DbContextOptions<CmsShoppingCartContext>>()))
+            {
+                if (context.Pages.Any())
+                {
+                    return;
+                }
+
+                context.Pages.AddRange(
+                    new Page
+                    {
+                        Title = "Home",
+                        Slug = "home",
+                        Content = "Home page",
+                        Sorting = 0
+                    },
+                    new Page
+                    {
+                        Title = "About Us",
+                        Slug = "about-us",
+                        Content = "About us page",
+                        Sorting = 100
+                    },
+                    new Page
+                    {
+                        Title = "Services",
+                        Slug = "services",
+                        Content = "services page",
+                        Sorting = 100
+                    },
+                    new Page
+                    {
+                        Title = "Contact",
+                        Slug = "contact",
+                        Content = "contact page",
+                        Sorting = 100
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
