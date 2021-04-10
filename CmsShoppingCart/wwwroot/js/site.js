9eb3dd7bb4814159c6ca26d42250d88038ee434d
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-04-11 00:35:36 +0600

    add additional func for adding product and show preview img before upload

--- CmsShoppingCart/wwwroot/js/site.js
+++ CmsShoppingCart/wwwroot/js/site.js
@@ -10,4 +10,17 @@
             $('div.alert.notification').fadeOut();
         }, 5000)
     }
-})
+});
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+
+        reader.onload = function (e) {
+            $("img#imgpreview").attr("src", e.target.result).width(200).height(200);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
