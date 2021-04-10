2d1ad3fd7fd6441037a56d0172ec03ede75c41bd
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-04-11 00:34:19 +0600

    rename controller

---
+++ CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,82 @@
+﻿using CmsShoppingCart.Infrastructure;
+using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly CmsShoppingCartContext _context;
+        private readonly IWebHostEnvironment _hostEnviroment;
+        public ProductsController(CmsShoppingCartContext context, IWebHostEnvironment hostEnviroment)
+        {
+            _context = context;
+            _hostEnviroment = hostEnviroment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.OrderByDescending(x => x.Id).ToListAsync());
+        }
+
+        // GET /admin/pages/create
+        public IActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(_context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
+            
+            return View();
+        }
+
+        // POST /admin/products/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            ViewBag.CategoryId = new SelectList(_context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
+
+                var slug = await _context.Products.FirstOrDefaultAsync(x => x.Slug == product.Slug);
+
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The product already exists.");
+                    return View(product);
+                }
+
+                string imageName = "noimage.png";
+
+                if (product.ImageUpload != null)
+                {
+                    var uploadDir = Path.Combine(_hostEnviroment.WebRootPath, "media/products");
+                    imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                    var filePath = Path.Combine(uploadDir, imageName);
+                    using var fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                }
+
+                product.Image = imageName;
+
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+
+                TempData["Success"] = "Product has been added!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+    }
+}
