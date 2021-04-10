eb7532a2fd82f560e12df10b1933a64bd1a37d1e
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-04-10 17:10:42 +0600

    add controller to admin area

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace CmsShoppingCart.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class PagesController : Controller
     {
-        public string Index()
+        private readonly CmsShoppingCartContext _context;
+        public PagesController(CmsShoppingCartContext context)
         {
-            return "test";
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var pages = from p in _context.Pages
+                        orderby p.Sorting
+                        select p;
+
+            var pagesList = await pages.ToListAsync();
+
+            return View(pagesList);
         }
     }
 }
