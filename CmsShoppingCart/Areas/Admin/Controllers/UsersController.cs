577e53fbc1c70be218dac6793aa18e18870432f6
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-04-22 01:49:36 +0600

    show users to admin area

---
+++ CmsShoppingCart/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<AppUser> userManger;
+
+        public UsersController(UserManager<AppUser> userManger)
+        {
+            this.userManger = userManger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(userManger.Users);
+        }
+    }
+}
