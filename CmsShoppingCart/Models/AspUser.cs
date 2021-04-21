11383029b06dd47cdc6790ab1709770f973483e9
Aybek Sultanov <aybek-sultanov@inbox.ru>
2021-04-21 23:43:13 +0600

    add identityUser

---
+++ CmsShoppingCart/Models/AspUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CmsShoppingCart.Models
+{
+    public class AspUser : IdentityUser
+    {
+        public string Occupation { get; set; }
+    }
+}
